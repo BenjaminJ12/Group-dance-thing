2c939d21219938a47a646204cafeff76ae5615cc
Ben Jones <clbj0910@glow.sch.uk>
2020-08-20 11:32:14 +0000

    complete(ish) internal com

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 #19/8/20
 #Hello Github
-#Int 2 practice
+#Initialise arrays
 nameArray = []  
 ticketArray = []
-
+#collect data
 groupName = input('What is the name of your group? ') 
 
-groupNumber = int(input('How many peple are in your group? Enter a number between 4 and 10 '))
+groupNumber = float(input('How many peple are in your group? Enter a number between 4 and 10 '))
 
-while groupNumber <4 or groupNumber >10:
+#input validation
+while groupNumber <4 or groupNumber >10 or not groupNumber.is_integer():
   print('Enter a number between 4 and 10 ')
-  groupNumber = int(input('How many peple are in your group? Enter a number between 4 and 10 '))
+  groupNumber = float(input('How many peple are in your group? Enter a number between 4 and 10 '))
 
+#make sure number of people is whole
+groupNumber = int(groupNumber)
+
+#loops the number of people in group
 for counter in range(groupNumber):
   name = input('Enter pupil name ')
   photo = input('Do you want a group photo? Enter yes or no ' )
   
+  #more input validation
   while photo != 'yes' and photo !='Yes' and photo !='no' and photo != 'No':
    print('Please enter yes or no ')
    photo = input('Do you want a group photo? Enter yes or no ' )
   
+  #Calculates ticket price
   if photo == 'no' or photo =='No':
     ticketPrice = 3.0
   elif photo =='yes'or photo =='Yes':
     ticketPrice = 4.99
  
+ #appends arrays
   nameArray.append(name)
  
   ticketArray.append(ticketPrice)
 
-  
-print('Group name: ', groupName)
+  #Prints outputs
+print('Group name:', groupName)
 print('Number in group: ', groupNumber)
 
 for counter in range(groupNumber):
